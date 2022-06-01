0be401bbea4682aed4bad4743e7548e3ceaa3127
Temirbek <atabekov.t.a@gmail.com>
2022-06-01 17:41:46 +0600

    second attempt

--- script.py
+++ script.py
@@ -1,5 +1,25 @@
 import pyautogui
+from pynput import mouse
 
+def send():
+    button7location = pyautogui.locateCenterOnScreen('unknown.png')
+    pyautogui.move(button7location)
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        send()
+
+# Collect events until released
+with mouse.Listener(
+        on_click=on_click,) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = mouse.Listener(
+    on_click=on_click,)
+listener.start()
+
+"""
 flag = True
 file = open("results.txt", "w")
 while(flag):
@@ -10,3 +30,4 @@ while(flag):
     else:
         file.close()
         break
+"""
